8d644edc02fedf6c47e12b273bf18513d8b81d0c
Oresto <orecto@gmail.com>
2016-06-07 22:16:46 +0300

    add QuestionDB and small refactoring

--- src/main/java/FileQuestionProvider.java
+++ src/main/java/FileQuestionProvider.java
@@ -4,42 +4,43 @@ import org.json.simple.parser.JSONParser;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Valko Serhii on 07-Jun-16.
  */
 public class FileQuestionProvider implements QuestionProvider {
 
-    private String filePath;
-    private Set<Question> questions = new HashSet<>();
-    public FileQuestionProvider(String filePath) {
-        this.filePath = filePath;
-    }
+  private String filePath;
+  private Set<Question> questions = new HashSet<>();
 
-    @Override
-    public Set<Question> getQuestions() {
-        JSONParser parser = new JSONParser();
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(filePath));
-            Object obj = parser.parse(br);
-            JSONArray array = (JSONArray)obj;
-            for (int i = 0; i < array.size(); i++){
-                JSONObject jsonObject = (JSONObject)array.get(i);
-                String question = (String)jsonObject.get("question");
-                List<String> answersList = new ArrayList<>();
+  public FileQuestionProvider(String filePath) {
+    this.filePath = filePath;
+  }
 
-                JSONArray quesArray = (JSONArray) jsonObject.get("answers");
-                for(int j = 0; j< quesArray.size(); j++ ){
-                    answersList.add((String) quesArray.get(j));
-                }
-                questions.add(new Question(question, answersList));
-            }
-        }catch(Exception pe){
+  @Override
+  public Set<Question> getQuestions() {
+    JSONParser parser = new JSONParser();
+    try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
+      Object obj = parser.parse(br);
+      JSONArray array = (JSONArray) obj;
+      for (Object o : array) {
+        JSONObject jsonObject = (JSONObject) o;
+        String question = (String) jsonObject.get("question");
+        JSONArray quesArray = (JSONArray) jsonObject.get("answers");
 
-            pe.printStackTrace();
-            System.out.println(pe);
+        List<String> answersList = new ArrayList<>(quesArray.size());
+        for (Object s : quesArray) {
+          answersList.add((String) s);
         }
-        return questions;
+        questions.add(new Question(question, answersList));
+      }
+    } catch (Exception pe) {
+      throw new RuntimeException(pe);
     }
+    return questions;
+  }
 }
--- src/main/java/QuestionAskerMenuImpl.java
+++ src/main/java/QuestionAskerMenuImpl.java
@@ -10,15 +10,28 @@ import java.util.Set;
  * Created by Oresto on 6/7/2016.
  */
 public class QuestionAskerMenuImpl implements QuestionAskerMenu {
+
+  private long start = 0;
+  private long endOfTest = 0;
+  private int rightAnswers = 0;
+  private int wrongAnswers = 0;
+
   @Override
   public void buildMenu(Set<Question> questions) {
     try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
       System.out.println("To quit type [q], for next question type [n]");
+      long timeForTestInSec = questions.size() * 30;
+      System.out.println("To pass exam you have :" + (timeForTestInSec / 60.0) + " mins for " + questions.size() + " questions!");
       System.out.println("Good luck!!!\n\n");
+      start = System.currentTimeMillis();
+      endOfTest = start + timeForTestInSec * 1000;
+
       for (Question question : questions) {
+        checkTime();
         List<String> answersToPublishing = printQuestionAndCheckAnswer(question, bufferedReader);
         waiting:
         while (true) {
+          checkTime();
           switch (checkTheAnswer(bufferedReader, question.getAnswer(), answersToPublishing)) {
             case NEXT: {
               System.out.println("\n\n");
@@ -26,8 +39,9 @@ public class QuestionAskerMenuImpl implements QuestionAskerMenu {
             }
             case QUIT:
               System.out.println("Bye-bye ;(");
-              System.exit(1);
+              exit();
             case TYPO:
+              checkTime();
               System.out.println("Please type number of the answer!!!");
           }
         }
@@ -35,6 +49,7 @@ public class QuestionAskerMenuImpl implements QuestionAskerMenu {
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+    exit();
   }
 
   private List<String> printQuestionAndCheckAnswer(Question question, BufferedReader bufferedReader) throws IOException {
@@ -73,17 +88,36 @@ public class QuestionAskerMenuImpl implements QuestionAskerMenu {
       if (answersToPublishing.size() >= selectedAnswer) {
         if (answersToPublishing.get(selectedAnswer).equalsIgnoreCase(originalAnswers.get(0))) {
           System.out.println("You are right!! :)");
+          rightAnswers++;
         } else {
           System.out.println("NOOOO!!! You are wrong");
           System.out.println("Right answer is " + originalAnswers.get(0));
           System.out.println("Be careful next time");
+          wrongAnswers++;
         }
+      }
+      else
+      {
+        return Options.TYPO;
       }
     }
     return Options.NEXT;
   }
 
   private enum Options {
-    NEXT, QUIT, TYPO;
+    NEXT, QUIT, TYPO
+  }
+
+  private void checkTime() {
+    if (System.currentTimeMillis() > endOfTest) {
+      System.out.println("TIME IS OUT :(");
+      exit();
+    }
+  }
+
+  private void exit() {
+    System.out.println("You've made: " + rightAnswers + " right answers");
+    System.out.println("You've made: " + wrongAnswers + " wrong answers");
+    System.exit(1);
   }
 }
