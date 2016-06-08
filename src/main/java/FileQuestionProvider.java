e3e21ea7acd851ce0e952a4d2eca6daee8fdbb91
valkosn <valkosn1987@gmail.com>
2016-06-08 11:45:27 +0300

    Asker: change JSON parser lib

--- src/main/java/FileQuestionProvider.java
+++ src/main/java/FileQuestionProvider.java
@@ -1,9 +1,10 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
-import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -14,33 +15,33 @@ import java.util.Set;
  */
 public class FileQuestionProvider implements QuestionProvider {
 
-  private String filePath;
-  private Set<Question> questions = new HashSet<>();
+    private String filePath;
+    private Set<Question> questions = new HashSet<>();
 
-  public FileQuestionProvider(String filePath) {
-    this.filePath = filePath;
-  }
-
-  @Override
-  public Set<Question> getQuestions() {
-    JSONParser parser = new JSONParser();
-    try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
-      Object obj = parser.parse(br);
-      JSONArray array = (JSONArray) obj;
-      for (Object o : array) {
-        JSONObject jsonObject = (JSONObject) o;
-        String question = (String) jsonObject.get("question");
-        JSONArray quesArray = (JSONArray) jsonObject.get("answers");
-
-        List<String> answersList = new ArrayList<>(quesArray.size());
-        for (Object s : quesArray) {
-          answersList.add((String) s);
-        }
-        questions.add(new Question(question, answersList));
-      }
-    } catch (Exception pe) {
-      throw new RuntimeException(pe);
+    public FileQuestionProvider(String filePath) {
+        this.filePath = filePath;
     }
-    return questions;
-  }
+
+    @Override
+    public Set<Question> getQuestions() {
+        JSONTokener jsonTokener;
+        try {
+            jsonTokener = new JSONTokener(new FileReader(filePath));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        JSONArray allJSONQuestions = new JSONArray(jsonTokener);
+
+        for (Object currObj : allJSONQuestions) {
+            JSONObject jsonObject = (JSONObject)currObj;
+            String question = jsonObject.getString("question");
+            JSONArray answers = new JSONArray(jsonObject.getJSONArray("answers").toString());
+            List<String> answersList = new ArrayList<>(answers.length());
+            for (Object currAnswer : answers) {
+                answersList.add((String)currAnswer);
+            }
+            questions.add(new Question(question, answersList));
+        }
+        return questions;
+    }
 }
