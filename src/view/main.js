8c5f73218fddb2341d29459fa49ecc16413edc31
Oresto <orecto@gmail.com>
2016-08-13 22:34:45 +0300

    AskerUI: super mega changes

--- src/view/main.js
+++ src/view/main.js
@@ -1,8 +1,8 @@
 /**
  * Created by valko on 13-Aug-16.
  */
-var incomeDate = data;
-var shuffledDate = shuffle(incomeDate);
+var incomeData = data;
+var shuffledData = shuffle(incomeData);
 var numberOfCurrantQuestion = 0;
 var trueCurrantAnswer;
 var totalTrueAnswers = 0;
@@ -19,11 +19,6 @@ function shuffle(a) {
     return a;
 }
 
-function getRandomQuestion(data) {
-    var randomNumber = Math.round(Math.random() * data.length);
-    return data[randomNumber.valueOf()];
-}
-
 function checkCurrantQuestion() {
     var radios = document.getElementsByName('answer');
     var userChoice;
@@ -36,61 +31,91 @@ function checkCurrantQuestion() {
         return false;
     } else if (trueCurrantAnswer === userChoice) {
         totalTrueAnswers++;
-        //alert("TRUE");
-    } else {
-        //alert("FALSE");
     }
 }
 
-function getNextQuestion() {
-    if (trueCurrantAnswer != null) {
-        checkCurrantQuestion();
-    } else {
-        var elem = document.getElementById("questionsAmount");
-        questionsAmount = elem.options[elem.selectedIndex].value;
-        elem.setAttribute("hidden", "true");
-        document.getElementById("next").setAttribute("value", "Next");
-        document.getElementById("questionTitle").innerHTML = "Question:";
-        document.getElementById("answersTitle").innerHTML = "Choice the answer:";
-    }
-    if (questionsAmount > numberOfCurrantQuestion) {
-        var questionObject = shuffledDate[numberOfCurrantQuestion];
-        var question = questionObject.question;
-        var answers = questionObject.answers;
-        trueCurrantAnswer = answers[0];
-        numberOfCurrantQuestion++;
-        renderQuestionAndAnswers(question, shuffle(answers));
-    } else {
-        document.getElementById("next").hidden = "true";
-        document.getElementById("questionTitle").innerHTML = "Result:"
-        var container = document.getElementById("answers");
-        clearContainer(container);
-        document.getElementById("answersTitle").setAttribute("hidden", "true");
-        var percents = Math.round(totalTrueAnswers / numberOfCurrantQuestion *100);
-        document.getElementById("question").innerHTML = "Your result " +
-            percents + "%. This is " + totalTrueAnswers + " of " + numberOfCurrantQuestion + " questions.";
-    }
+function startTest() {
+    document.getElementById("start_screen_holder").hide();
+    getNextQuestion(1);
+
+    // if (trueCurrantAnswer != null) {
+    //     checkCurrantQuestion();
+    // } else {
+    //     var elem = document.getElementById("questionsAmount");
+    //     questionsAmount = elem.options[elem.selectedIndex].value;
+    //     elem.hide();
+    //     document.getElementById("next").setAttribute("value", "Next");
+    //     document.getElementById("questionTitle").innerHTML = "Question:";
+    //     document.getElementById("answersTitle").innerHTML = "Choice the answer:";
+    // }
+    // if (questionsAmount > numberOfCurrantQuestion) {
+    //     var questionObject = shuffledData[numberOfCurrantQuestion];
+    //     var question = questionObject.question;
+    //     var answers = questionObject.answers;
+    //     trueCurrantAnswer = answers[0];
+    //     numberOfCurrantQuestion++;
+    //     renderQuestionAndAnswers(question, shuffle(answers));
+    // } else {
+    //     document.getElementById("next").hide();
+    //     document.getElementById("questionTitle").innerHTML = "Result:";
+    //     var container = document.getElementById("answers");
+    //     clearContainer(container);
+    //     document.getElementById("answersTitle").hide();
+    //     var percents = Math.round(totalTrueAnswers / numberOfCurrantQuestion *100);
+    //     document.getElementById("question").innerHTML = "Your result " +
+    //         percents + "%. This is " + totalTrueAnswers + " of " + numberOfCurrantQuestion + " questions.";
+    // }
 }
 
-function renderQuestionAndAnswers(question, answers) {
+function getCorrectAnswer(questionNumber) {
+    return shuffledData[questionNumber].answer[0];
+}
 
-    var questionBox = document.getElementById("question");
-    questionBox.disabled = false;
+function getNextQuestion(questionNumber){
+    var questionObject = shuffledData[numberOfCurrantQuestion];
+    var question = questionObject.question;
+    var answers = questionObject.answers.clone();
+    renderQuestionAndAnswers(question, shuffle(answers), questionNumber);
+}
+
+Element.prototype.hide = function(){
+    this.setAttribute("hidden", "true");
+};
+
+Element.prototype.show = function(){
+    this.setAttribute("hidden", "false");
+};
+
+Array.prototype.clone = function() {
+    return this.slice(0);
+};
+
+function cloneAndAppendElementById(targetId, destinationId, idForCopy) {
+    var node = document.getElementById(targetId).cloneNode(true);
+    node.id = idForCopy;
+    var destinationElement = document.getElementById(destinationId);
+    destinationElement.appendChild(node);
+    return document.getElementById(idForCopy);
+}
+
+function renderQuestionAndAnswers(question, answers, questionNumber) {
+    var idForCopyQuestion = 'qa_' + questionNumber;
+    var questionBox = cloneAndAppendElementById("qa_holder_template", "test_answers", idForCopyQuestion);
     questionBox.innerHTML = question.toString();
 
     var number = answers.length;
-    var container = document.getElementById("answers");
-
-    clearContainer(container)
-
+    var questionContainer = document.getElementById(idForCopyQuestion);
+    var answerTemplate = questionContainer.getElementsByTagName("label");
     for (var i = 0; i < number; i++) {
-        var input = document.createElement("input");
-        input.type = "radio";
-        input.name = "answer";
+        var currentAnswer = answerTemplate;
+        if(i <= (number - 1)){
+            var idForCopyAnswer = 'answer_' + questionNumber;
+            currentAnswer = cloneAndAppendElementById("answers", idForCopyQuestion, idForCopyAnswer);
+        }
+        var input = currentAnswer.getElementsByName("answerContainer");
+        var span = currentAnswer.getElementById("answerText");
         input.setAttribute("value", answers[i]);
-        container.appendChild(input);
-        container.appendChild(document.createTextNode(answers[i]));
-        container.appendChild(document.createElement("br"));
+        span.innerHTML = answers[i];
     }
 }
 
@@ -98,8 +123,8 @@ function addMaxValueToQuestionAmount() {
     var container = document.getElementById("questionsAmount");
     if (container.length <= 5) {
         var elem = document.createElement("option");
-        elem.setAttribute("value", incomeDate.length.toString());
-        elem.innerHTML = incomeDate.length.toString() + " questions";
+        elem.setAttribute("value", incomeData.length.toString());
+        elem.innerHTML = incomeData.length.toString() + " questions";
         container.appendChild(elem);
     }
 }
